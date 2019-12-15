c6ad57e295b424305c77793566b3ea4428c2533e
Stacy Kim <stacy.kim@ucla.edu>
2019-12-15 11:08:33 -0600

    Disable concurrent webpack-dev-server port and allow service to run on single port 3002

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -17,7 +17,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://localhost:3500/artists/8")
+    fetch("http://localhost:3002/artists/8")
       .then(response => response.json())
       .then(responseData => {
         this.setState({
